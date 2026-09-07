ed4261227368c3633c6d2e5d15b9bb1391836a97
harshkr2926-star <harshkr2926@gmail.com>
2026-09-08 00:52:39 +0530

    Funstion added to release seat after 10 minutes

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -47,5 +47,28 @@ const syncUserUpdation = inngest.createFunction(
     }
 )
 
+const releaseSeatsAndDeleteBooking = inngest.createFunction(
+    {id: 'release-seats-delete-booking',
+    triggers:[{event: "app/checkpayment"}]},
+    async ({ event, step })=>{
+        const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+        await step.sleepUntil('wait-for-10-minutes', tenMinutesLater);
 
-export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation];
+        await step.run('check-payment-status', async ()=>{
+            const bookingId = event.data.bookingId;
+            const booking = await Booking.findById(bookingId)
+
+            // If payment is not made, release seats and delete booking
+            if(!booking.isPaid){
+                const show = await Show.findById(booking.show);
+                booking.bookedSeats.forEach((seat)=>{
+                    delete show.occupiedSeats[seat]
+                });
+                show.markModified('occupiedSeats')
+                await show.save()
+                await Booking.findByIdAndDelete(booking._id)
+            }
+        })
+    }
+)
+export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation,releaseSeatsAndDeleteBooking];
