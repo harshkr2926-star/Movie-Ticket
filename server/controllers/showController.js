3c5b25e4c3187a7ba939d5d7476f8c2db5290c20
harshkr2926-star <harshkr2926@gmail.com>
2026-09-08 16:29:13 +0530

    Project Completed

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,15 +1,15 @@
 import axios from "axios"
-
 import Movie from "../models/movie.js";
 import Show from "../models/Show.js";
 import { inngest } from "../inngest/index.js";
+
+// API to get now playing movies from TMDB API
 export const getNowPlayingMovies = async (req, res)=>{
     try {
-        const {data} =await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
-            headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`,
-        accept:"application/json"},
-        timeout:5000
+        const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+            headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`}
         })
+
         const movies = data.results;
         res.json({success: true, movies: movies})
     } catch (error) {
@@ -18,30 +18,27 @@ export const getNowPlayingMovies = async (req, res)=>{
     }
 }
 
+// API to add a new show to the database
 export const addShow = async (req, res) =>{
     try {
         const {movieId, showsInput, showPrice} = req.body
+
         let movie = await Movie.findById(movieId)
 
-        if(!movie){
-            const [movieDetailsResponse, movieCreditsResponse]= await Promise.all([
-                axios.get(`https://api.themoviedb.org/3/movie/${movieId}`,{
-           headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`,
-        accept:"application/json"},
-       timeout:5000
-        
-        }),
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`,{
-             headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`,
-        accept:"application/json"},
-         timeout:5000
-        })
+        if(!movie) {
+            // Fetch movie details and credits from TMDB API
+            const [movieDetailsResponse, movieCreditsResponse] = await Promise.all([
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
+            headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`} }),
+
+                axios.get(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
+            headers: {Authorization : `Bearer ${process.env.TMDB_API_KEY}`} })
             ]);
 
             const movieApiData = movieDetailsResponse.data;
             const movieCreditsData = movieCreditsResponse.data;
 
-            const movieDetails = {
+             const movieDetails = {
                 _id: movieId,
                 title: movieApiData.title,
                 overview: movieApiData.overview,
@@ -55,8 +52,9 @@ export const addShow = async (req, res) =>{
                 vote_average: movieApiData.vote_average,
                 runtime: movieApiData.runtime,
              }
+
              // Add movie to the database
-                movie = await Movie.create(movieDetails);
+             movie = await Movie.create(movieDetails);
         }
 
         const showsToCreate = [];
@@ -74,21 +72,23 @@ export const addShow = async (req, res) =>{
         });
 
         if(showsToCreate.length > 0){
-                    await Show.insertMany(showsToCreate);
-                }
-        
-                 //  Trigger Inngest event
-                 
-        
-                res.json({success: true, message: 'Show Added successfully.'})
+            await Show.insertMany(showsToCreate);
+        }
 
+         //  Trigger Inngest event
+         await inngest.send({
+            name: "app/show.added",
+             data: {movieTitle: movie.title}
+         })
 
+        res.json({success: true, message: 'Show Added successfully.'})
     } catch (error) {
         console.error(error);
         res.json({success: false, message: error.message})
     }
 }
 
+// API to get all shows from the database
 export const getShows = async (req, res) =>{
     try {
         const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({ showDateTime: 1 });
--- server/server.js
+++ server/server.js
@@ -4,32 +4,35 @@ import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import {inngest, functions } from './inngest/index.js';
+import { inngest, functions } from "./inngest/index.js"
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
-import userRouter from './routes/useRoutes.js';
+import userRouter from './routes/userRoutes.js';
 import { stripeWebhooks } from './controllers/stripeWebhooks.js';
-const app=express();
-const PORT=3000;
+
+const app = express();
+const port = 3000;
 
 await connectDB()
 
-//Stripe webhook route
-app.use('/api/stripe',express.raw({type:'application/json'}),stripeWebhooks)
+// Stripe Webhooks Route
+app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
-//MIDDLEWARE
-
+// Middleware
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
 
-//API Routes
-app.get('/',(req,res)=> res.send('Server is Live!'))
-app.use('/api/inngest',serve({client:inngest,functions}))
-app.use('/api/show',showRouter)
-app.use('/api/booking',bookingRouter)
-app.use('/api/admin',adminRouter)
-app.use('/api/user',userRouter)
 
-app.listen(PORT,()=> console.log(`Server is listening at http://localhost:${PORT}`));
+// API Routes
+app.get('/', (req, res)=> res.send('Server is Live!'))
+app.use('/api/inngest', serve({ client: inngest, functions }))
+app.use('/api/show', showRouter)
+app.use('/api/booking', bookingRouter)
+app.use('/api/admin', adminRouter)
+app.use('/api/user', userRouter)
+
+
+app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`));
+
